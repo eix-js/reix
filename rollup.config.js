9f2197380a714d6568446aa6f7a6d1a5537f1948
Mateiadrielrafael <rafaeladriel11@gmail.com>
2019-09-22 18:01:10 +0300

    added declaration generation to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,47 @@
+// @ts-check
+
 import babel from 'rollup-plugin-babel'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import npmConfig from './package.json'
+import dts from 'rollup-plugin-dts'
 import { terser } from 'rollup-plugin-terser'
+
+// @ts-ignore
+import npmConfig from './package.json'
 
 const dist = './dist'
 const dev = Boolean(process.env.ROLLUP_WATCH)
 const babelConfig = require('./babel.config.js')
+const input = './src/index.ts'
 
-export default {
-    input: './src/index.ts',
-    output: [
-        {
-            file: `${dist}/bundle.cjs.js`,
-            format: 'cjs'
-        },
-        {
-            file: `${dist}/bundle.esm.js`,
-            format: 'esm'
-        }
-    ],
-    external: [...Object.keys(npmConfig.dependencies || {})],
-    plugins: [
-        nodeResolve({
-            extensions: ['.ts']
-        }),
-        babel({
-            exclude: 'node_modules/**',
-            extensions: ['ts', 'js'],
-            ...babelConfig
-        }),
-        !dev && terser()
-    ]
-}
+export default [
+    {
+        input,
+        output: [
+            {
+                file: `${dist}/bundle.cjs.js`,
+                format: 'cjs'
+            },
+            {
+                file: `${dist}/bundle.esm.js`,
+                format: 'esm'
+            }
+        ],
+        external: [...Object.keys(npmConfig.dependencies || {})],
+        plugins: [
+            nodeResolve({
+                extensions: ['.ts']
+            }),
+            babel({
+                exclude: 'node_modules/**',
+                extensions: ['ts', 'js'],
+                ...babelConfig
+            }),
+            !dev && terser()
+        ]
+    },
+    {
+        input,
+        output: [{ file: `${dist}/index.d.ts`, format: 'es' }],
+        plugins: [dts()]
+    }
+]
