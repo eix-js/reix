5572a12661c441d569d1bee22c54bf756ea39c73
Matei Adriel <rafaeladriel11@gmail.com>
2019-10-11 15:28:11 +0300

    fix(@reix/ecs): Fixed index.ts exports
    In the previous version I forgot to update the exports of the ecs
    package, fixed now:)
    
    Closes #1

--- packages/ecs/src/index.ts
+++ packages/ecs/src/index.ts
@@ -1,4 +1,12 @@
 export * from './classes/EntityPool'
 export * from './classes/IndexedComponentManager'
+export * from './classes/ComponentDiff'
+export * from './classes/Ecs'
+
+export * from './constants/componentEvents'
+export * from './constants/componentOperations'
 
 export * from './types/ComponentManager'
+export * from './types/ComponentManager'
+export * from './types/ComponentMap'
+export * from './types/ComponentOperation'
